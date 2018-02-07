f88d2e96854312faef83a795ea032080e4c6b958
Hoon Lee <yl.hoony@gmail.com>
2018-02-07 18:20:10 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,6 +7,6 @@ class Song < ActiveRecord::Base
 
   def artist_name=(name)
     binding.pry
-    self.artist = find_or_create_by(name: name)
+    self.artist = Artist.find_or_create_by(name: name)
   end
 end
